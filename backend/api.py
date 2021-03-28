3e08f2bb6c5ca60ece5e97eaa303b718c7fa3f4e
Jia Qi Poon <jqp18@ic.ac.uk>
2021-03-28 17:22:54 +0800

    Clean up files and add requirements.txt

--- backend/api.py
+++ backend/api.py
@@ -8,27 +8,7 @@ def get_db_connection():
     c.row_factory = lambda cursor, row: {'foo': row[0]}
     return conn
 
-def get_post(post_id):
-    conn = get_db_connection()
-    post = conn.execute('SELECT * FROM posts WHERE id = ?',
-                        (post_id,)).fetchone()
-    conn.close()
-    if post is None:
-        abort(404)
-    return post
-
 app = Flask(__name__)
-
-global count
-count = 0
-@app.route('/button', methods=['GET', 'POST'])
-def button():
-    if request.method == 'POST':
-        global count
-        count = count + 1
-        return render_template('button.html', ButtonPressed=count)
-    
-    return render_template('button.html', ButtonPressed=count)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -48,8 +28,3 @@ def data():
         return render_template('data.html', items=items)
 
     return render_template('data.html', items=[])
-
-@app.route('/<int:post_id>')
-def post(post_id):
-    post = get_post(post_id)
-    return render_template('post.html', post=post)
